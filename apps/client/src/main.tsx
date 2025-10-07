264f13efba1f76b8391ad813bccde7ee842511b9
jteillard <jimmy@tempeos.com>
2025-10-07 14:21:43 +0200

    feat: refactor TanStack Query integration and move provider to a new module

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,16 +1,16 @@
 import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import * as TanStackQueryProvider from './integrations/tanstack-query/root-provider.tsx'
 import { routeTree } from './routeTree.gen'
 import './styles.css'
+import { TanStackQuery } from './lib/query.tsx'
 import reportWebVitals from './reportWebVitals.ts'
 
-const TanStackQueryProviderContext = TanStackQueryProvider.getContext()
+const tsQueryContext = TanStackQuery.getContext()
 const router = createRouter({
   routeTree,
   context: {
-    ...TanStackQueryProviderContext,
+    ...tsQueryContext,
   },
   defaultPreload: 'intent',
   scrollRestoration: true,
@@ -29,9 +29,9 @@ if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <TanStackQueryProvider.Provider {...TanStackQueryProviderContext}>
+      <TanStackQuery.Provider {...tsQueryContext}>
         <RouterProvider router={router} />
-      </TanStackQueryProvider.Provider>
+      </TanStackQuery.Provider>
     </StrictMode>,
   )
 }
